3673a0548080bdb86610f746ae60f56f52dde2b5
Steve <bevets87@gmail.com>
2017-05-30 05:02:05 -0400

    modified config file

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,7 @@
 export default {
-  TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY || 'aYr9AomYeRm7d9YIv1Jrvrii7',
-  TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET || 'n1rSBLhlXyrbII5ZKZpK7tgc4P4hbaKwP9AIj04mBhkSdclK2A',
-  JWT_SECRET: process.env.JWT_SECRET || 'This Is The Secret Token String',
-  SESSION_SECRET: process.env.SESSION_SECRET || 'SuperSecretSessionSecret',
-  TWITTER_CALLBACK_URL: process.env.TWITTER_CALLBACK_URL || 'http://localhost:3000/auth/twitter/callback'
+  TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY /*|| 'aYr9AomYeRm7d9YIv1Jrvrii7'*/,
+  TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET /*|| 'n1rSBLhlXyrbII5ZKZpK7tgc4P4hbaKwP9AIj04mBhkSdclK2A'*/,
+  JWT_SECRET: process.env.JWT_SECRET /*|| 'This Is The Secret Token String'*/,
+  SESSION_SECRET: process.env.SESSION_SECRET /*|| 'SuperSecretSessionSecret'*/,
+  TWITTER_CALLBACK_URL: process.env.TWITTER_CALLBACK_URL /*|| 'http://localhost:3000/auth/twitter/callback'*/
 }
